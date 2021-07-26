e1261ef45e67c772cac0bc14459247b268721ac3
maheshnawale <Maheshnawale123@gmail.com>
2021-07-26 10:39:56 +0530

    first commit for dummy admissions

---
+++ admissions-service/src/main/java/com/mahesh/admissionsservice/AdmissionsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mahesh.admissionsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdmissionsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdmissionsServiceApplication.class, args);
+	}
+
+}
